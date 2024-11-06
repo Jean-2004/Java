4e6bcc1b9f401fb40faf3ac4094e7011909eaa34
MaxxxStar <jeanlipefs@gmail.com>
2024-11-06 18:46:30 -0300

    removed clear

--- Main.java
+++ Main.java
@@ -5,11 +5,6 @@ public class Main {
         menu();
     }
 
-    public static void clear() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();  
-    }
-
     public static void menu() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
